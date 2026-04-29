9cd76ad602b83e4894992c2753458432669ab690
Wasayullah <wasayullahmohammad@gmail.com>
2026-04-29 05:23:53 -0700

    Auto update

--- app.js
+++ app.js
@@ -1081,3 +1081,12 @@ var series = "";
 for(var i=1; i<=15; i++){
     counting+= `${i}, `
 } 
+for(var i=10; i>=1; i--){
+    reverseCounting+= `${i}, `
+} 
+for(var i=0; i<=20; i+=2){
+    even+= `${i}, `
+}  
+for(var i=1; i<=20; i+=2){
+    odd+= `${i}, `
+}
