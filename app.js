e6ff945ba171292d3f1e706bf015821a6115e4f2
Wasayullah <wasayullahmohammad@gmail.com>
2026-04-29 14:25:05 -0700

    Auto update

--- app.js
+++ app.js
@@ -1284,7 +1284,7 @@ document.getElementById("ch-21_question_13").innerHTML = output;
 // Write a program to enable “search by user input” in an array. After searching, prompt the user whether the given item is found in the list or not. 
 // Note: Perform case insensitive search. Whether the user enters cookie, Cookie, COOKIE or coOkIE, program should inform about its availability.
 
-var userInput = prompt("Welcome to ABC Bakery. What do you want to order?").toLowerCase();
+var UserInput = prompt("Welcome to ABC Bakery. What do you want to order?").toLowerCase();
 var  A = ["cake", "apple pie", "cookie", "chips", "patties"] ;
 var found = false;
 for(i=0;i<A.length;i++){
@@ -1295,8 +1295,8 @@ for(i=0;i<A.length;i++){
 }
 if(found){
     var output = `<p class = "fs-4 text-success">${UserInput} is available at index ${i} </p>`;
-    document.getElementById("ch-21_question_14").innerHTML = output;
+    
 }else{
     output = `<p class = "fs-4 text-danger">Sorry ${UserInput} is not available at our backery</p>`;
-    document.getElementById("ch-21_question_14").innerHTML = output;
 }
+document.getElementById("ch-21_question_14").innerHTML = output;
