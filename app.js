269bd31834df43425ee7ef5704e5dc5c5fab68f6
Wasayullah <wasayullahmohammad@gmail.com>
2026-04-29 12:30:22 -0700

    Auto update

--- app.js
+++ app.js
@@ -1174,4 +1174,7 @@ Chapters - 21 to 25 : STRING METHODS
 // 1.  Write a program that takes two user inputs for first and  last name using prompt and merge them in a new variable  titled fullName. Greet the user using his full name.
 
 var firstName = prompt("Enter your first name")
-var firstName = prompt("Enter your first name")
+var lastName = prompt("Enter your last name")
+var fullName = firstName+lastName;
+let output = `<p class = "fs-4 ">${multiples.join(", ")}</p>`;
+document.getElementById("ch-21_question_10").innerHTML = output;
