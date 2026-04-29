1070c167e24d1bde147e38fea08a36c60b8cb4e2
Wasayullah <wasayullahmohammad@gmail.com>
2026-04-29 05:20:41 -0700

    Auto update

--- app.js
+++ app.js
@@ -1072,4 +1072,4 @@ document.getElementById("ch-17_question_5").innerHTML = output;
 // b. Reverse counting: 10, 9, 8, 7, 6, 5, 4, 3, 2, 1 
 // c. Even: 0, 2, 4, 6, 8, 10, 12, 14, 16, 18, 20 
 // d. Odd: 1, 3, 5, 7, 9, 11, 13, 15, 17, 19 
-//e. Series: 2k, 4k, 6k, 8k, 10k, 12k, 14k, 16k, 18k, 20k
+// e. Series: 2k, 4k, 6k, 8k, 10k, 12k, 14k, 16k, 18k, 20k
