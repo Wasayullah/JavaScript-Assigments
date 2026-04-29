d40ca21b09edbca4f1bfddc57a49a7fb40514d5e
Wasayullah <wasayullahmohammad@gmail.com>
2026-04-29 13:04:14 -0700

    Auto update

--- app.js
+++ app.js
@@ -1200,9 +1200,12 @@ let str ="Hello World";
 let requiredIndex = str.lastIndexOf("l")
 let output = `<p class = "fs-4 "> string: ${str}<br>Index of 'l': ${requiredIndex}</p>`;
 document.getElementById("ch-21_question_4").innerHTML = output;
-*/
+
 // 5. Write a program to find the character at 3rd index in the word “Pakistani” and display the result in your browser.
 let str ="Pakistani";
 let requiredChar = str.charAt(3)
-let output = `<p class = "fs-4 "> string: ${str}<br>Character at index 3:  ${ requiredChar}</p>`;
+let output = `<p class = "fs-4 "> string: ${str}<br>Character at index 3:  ${requiredChar}</p>`;
 document.getElementById("ch-21_question_5").innerHTML = output;
+
+*/
+// 6. Repeat Q1 using string concat() method.
