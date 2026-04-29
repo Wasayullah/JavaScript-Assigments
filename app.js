4aa29f142af7887ab6207839b4ddbc895e91ddc5
Wasayullah <wasayullahmohammad@gmail.com>
2026-04-29 13:30:49 -0700

    Auto update

--- app.js
+++ app.js
@@ -1243,7 +1243,7 @@ document.getElementById("ch-21_question_8").innerHTML = output;
 */
 //10. Write a program that takes user input. Convert and show the input in capital letters.
 var userInput = prompt("Write any thing");
-if(userInput == isd){
+if(typeof userInput!=="string"){
     return
 }
 var UpperCase = userInput.toUpperCase();
