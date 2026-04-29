8cac466471f3a536eecfbd1965f2ef033ac1f4d8
Wasayullah <wasayullahmohammad@gmail.com>
2026-04-29 12:02:23 -0700

    Auto update

--- app.js
+++ app.js
@@ -1155,4 +1155,6 @@ output += `<p class = "fs-4 ">The smallest number in the array is ${smallest}</p
 document.getElementById("ch-17_question_9").innerHTML = output;
 */
 // 10. Write a program to print multiples of 5 ranging 1 to  100.   
- 
+ for(i=1;i<=100;i++){
+    if(i%5==0)
+ }
