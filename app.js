c1463cfa75cb2812a18e740271c57082efc36e1b
Wasayullah <wasayullahmohammad@gmail.com>
2026-04-29 13:12:43 -0700

    Auto update

--- app.js
+++ app.js
@@ -1219,7 +1219,7 @@ document.getElementById("ch-21_question_6").innerHTML = output;
 // 7. Write a program to replace the “Hyder” to “Islam” in the word “Hyderabad” and display the result in your browser
 
 let city = "Hyderabad";
-let replaced = city.replace("Hyder,Islam");
+let replaced = city.replace("Hyder","Islam");
 let output = `<p class = "fs-4 ">City: ${city} 🫡</p>`;
 let output = `<p class = "fs-4 ">After replacement: ${replaced} 🫡</p>`;
 document.getElementById("ch-21_question_7").innerHTML = output;
