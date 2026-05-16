c655fff5e57067ec080238b937e538fd8b87858e
Wasayullah <wasayullahmohammad@gmail.com>
2026-05-16 02:40:54 -0700

    Auto update

--- app.js
+++ app.js
@@ -1355,4 +1355,4 @@ for (i = 0; i < array.length; i++) {
 // //17. Write a program to display the last character of a user input
 var UserInput = document.getElementById("inp").value;
 var LastWord = UserInput.slice(0,3)
-document.write(LastWord)
+console.log(LastWord)
