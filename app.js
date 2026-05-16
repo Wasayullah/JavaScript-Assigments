c0342a08a0a0c0024ad63d1e62b5dc3610a2c681
Wasayullah <wasayullahmohammad@gmail.com>
2026-05-16 01:06:47 -0700

    Auto update

--- app.js
+++ app.js
@@ -1338,3 +1338,9 @@ function check() {
 }
 
 */
+
+//16. Write a program to convert the following string to an array using string split method.
+//var university = “University of Karachi”;
+//Display the elements of array in your browser.
+
+var university = “University of Karachi";
