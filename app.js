8852cc4540c9aeab4bf2bd0d2ade7055fd4fe2a8
Mohammad wasayullah <wasayullahmohammad@gmail.com>
2026-08-21 23:58:52 +0500

    completed latest assignments

--- app.js
+++ app.js
@@ -1734,5 +1734,5 @@ document.getElementById("ch-35_question_4").innerHTML = output;
 const Square = (a)=>a**2;
 let num = Number(prompt("Enter any number \nto get its square"))
 let sq = Square(num)
-output = `<h1>Calculator</h1><p class="fs-4">Square of ${num}is :${sq}</p>`
+output = `<h1>Calculator</h1><p class="fs-4">Square of ${num} is :${sq}</p>`
 document.getElementById("ch-35_question_5").innerHTML = output;
