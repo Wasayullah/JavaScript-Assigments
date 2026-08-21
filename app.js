f42a3965edf3bfa8b550fe25a7232c733fe52ce4
Mohammad wasayullah <wasayullahmohammad@gmail.com>
2026-08-22 00:34:20 +0500

    completed latest assignments

--- app.js
+++ app.js
@@ -1759,12 +1759,12 @@ let table = (a,b)=>{
     for(let i=1; i<=a ;i++){
         result += i*b;
         let output
-        output += `<p class="fs-4">${num} x ${i}<br></p>`
+        output += `<p class="fs-4">${num} x ${i} = ${result}</p>`
     }
-    
+    return output
 };
 let num = Number(prompt("Enter a number to find Multiplication table"));
 let range = Number(prompt("Enter the range",10))
 let tab = table(num,range)
-output += `<p class="fs-4">Multiplication table of ${num}: is ${tab}<br></p>`
+// output += `<p class="fs-4">Multiplication table of ${num}: is ${tab}<br></p>`
 document.getElementById("ch-35_question_7").innerHTML = output;
