ecde1dc0f02d5aa16bcedd9300d9a4dc418b5673
Mohammad wasayullah <wasayullahmohammad@gmail.com>
2026-08-22 00:19:17 +0500

    completed latest assignments

--- app.js
+++ app.js
@@ -1738,8 +1738,9 @@ document.getElementById("ch-35_question_5").innerHTML = output;
 */
 // 6. Write a function that computes factorial of a number. 
  const factorial = (a)=>{
+    let result=1
     for(let i=1 ; i>=a ;i++){
-       return i*i
+       result *=i
     }
  }
  let num = Number(prompt("Enter a number to find its factorial"));
