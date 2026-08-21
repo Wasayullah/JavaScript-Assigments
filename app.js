61d89337094547f125c79c54c709e49b9f256087
Mohammad wasayullah <wasayullahmohammad@gmail.com>
2026-08-21 23:58:56 +0500

    fix output format in calculator to include space before 'is'

--- app.js
+++ app.js
@@ -1729,10 +1729,10 @@ let answer = Calculate(num_1,operator,num_2);
 output = `<h1>Calculator</h1><p class="fs-4">${num_1} ${operator} ${num_2}  : ${answer}</p>`
 document.getElementById("ch-35_question_4").innerHTML = output;
 
-*/
 // 5. Write a function that squares its argument.
 const Square = (a)=>a**2;
 let num = Number(prompt("Enter any number \nto get its square"))
 let sq = Square(num)
 output = `<h1>Calculator</h1><p class="fs-4">Square of ${num} is :${sq}</p>`
 document.getElementById("ch-35_question_5").innerHTML = output;
+*/
