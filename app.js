ce6f31f27fa9add8a21860deceb0f5854b7e6337
Mohammad wasayullah <wasayullahmohammad@gmail.com>
2026-08-23 00:11:06 +0500

    completed latest assignments

--- app.js
+++ app.js
@@ -1826,5 +1826,5 @@ const palindrome = (string) => {
     return output
 }
 let string = prompt("Enter any word").toLowerCase();
-output=
+output = palindrome(string)
 document.getElementById("ch-35_question_9").innerHTML = output;
