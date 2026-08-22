3f37001ed765e534fe318a027ea776fe5de077bd
Mohammad wasayullah <wasayullahmohammad@gmail.com>
2026-08-22 08:51:51 +0500

    completed latest assignments

--- app.js
+++ app.js
@@ -1787,4 +1787,5 @@ const calculateHypotenuse = (a,b)=>{
 let prep = Number(prompt("Length of prependicular"));
 let base = Number(prompt("Length of base"))
 let hyp = calculateHypotenuse(prep,base)
+        output += `<p class="fs-4">Hypotenuse: ${hyp}</p>`
 document.getElementById("ch-35_question_8").innerHTML = output;
