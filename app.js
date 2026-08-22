a547cb20e42e70579c03628247530687fb13cf51
Mohammad wasayullah <wasayullahmohammad@gmail.com>
2026-08-22 23:36:38 +0500

    completed latest assignments

--- app.js
+++ app.js
@@ -1790,6 +1790,7 @@ const calculateHypotenuse = (a,b)=>{
 
     let c = Math.sqrt(a_sq + b_sq);
     return c
+    console.log(c)
 };
 let prep = Number(prompt("Length of prependicular"));
 let base = Number(prompt("Length of base"))
