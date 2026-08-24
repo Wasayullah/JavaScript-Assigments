23ae28955297f001eaadfef4f2d8eeab6b046cdc
Mohammad wasayullah <wasayullahmohammad@gmail.com>
2026-08-25 00:03:42 +0500

    completed latest assignments

--- app.js
+++ app.js
@@ -1836,7 +1836,7 @@ document.getElementById("ch-35_question_10").innerHTML = output;
 // EXPECTED OUTPUT : 'The Quick Brown Fox'
 function capitalizeWords(str) {
     return str
-        .toLoweCase()
+        .toLowerCase()
         .split(" ")
         .map(word => word.charAt(0).toUpperCase() + word.slice(1))
         .join(" ");
