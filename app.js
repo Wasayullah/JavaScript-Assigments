e1ac2a41804ecd61d13ec7ea4c331b1e85150dc9
Mohammad wasayullah <wasayullahmohammad@gmail.com>
2026-08-25 23:45:32 +0500

    completed latest assignments

--- app.js
+++ app.js
@@ -1888,20 +1888,23 @@ const letter = prompt("Enter a letter")
 
 const output = `<p class="fs-4">The number of occurrences of "${letter}" in "${string}" is : ${countLetter(string,letter)}</p>`
 document.getElementById("ch-35_question_13").innerHTML = output;
-*/
-/*
-14. The Geometrizer 
-Create 2 functions that calculate properties of a circle, using 
-the definitions here. 
-Create a function called calcCircumference: 
-• Pass the radius to the function. 
-• Calculate the circumference based on the radius, and output 
-"The circumference is NN". 
-Create a function called calcArea: 
-• Pass the radius to the function. 
-• Calculate the area based on the radius, and output "The area 
-is NN". 
- 
-Circumference of circle    =     2πr 
-Area of circle       =     πr*/ 
 
+// 14. The Geometrizer 
+// Create 2 functions that calculate properties of a circle, using 
+// the definitions here. 
+// Create a function called calcCircumference: 
+// • Pass the radius to the function. 
+// • Calculate the circumference based on the radius, and output 
+// "The circumference is NN". 
+// Create a function called calcArea: 
+// • Pass the radius to the function. 
+// • Calculate the area based on the radius, and output "The area 
+// is NN". 
+//  
+// Circumference of circle    =     2πr 
+// Area of circle       =     πr2
+*/
+
+const  calcCircumference =() =>{
+    
+}
