8373d6cd393d8d77a04ce0703ad182599e8f6874
Mohammad wasayullah <wasayullahmohammad@gmail.com>
2026-08-28 23:49:59 +0500

    completed latest assignments

--- app.js
+++ app.js
@@ -2011,10 +2011,9 @@ const string = prompt("Enter any string").trim().toLowerCase()
 const ch = prompt("Enter any character to fin its index-no.")
 const arr = string.split('')
 const index = string.indexOf(ch)
-output = `<p class = "fs-4">Index of "${ch}" in "${string}" is : ${index} </p>`
 if(index==-1){
+    output = `<p class = "fs-4">${ch} not found in the string: ${string.join()} </p>`
 }else{
-
-    output = `<p class = "fs-4">${ch} not found in the string: ${string} </p>`
+    output = `<p class = "fs-4">Index of "${ch}" in "${string.join()}" is : ${index} </p>`
 }
 document.getElementById("ch-38_question_5").innerHTML = output;
