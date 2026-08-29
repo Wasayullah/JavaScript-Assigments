4751c445ef0fc15edc8b9513c22fd6ae5411cdd8
Mohammad wasayullah <wasayullahmohammad@gmail.com>
2026-08-29 23:53:42 +0500

    completed latest assignments

--- app.js
+++ app.js
@@ -2025,3 +2025,5 @@ document.getElementById("ch-38_question_5").innerHTML = output;
 
 // 6. Write a function to delete all vowels from a sentence. Assume 
 // that the sentence is not more than 25 characters long. 
+
+const 
