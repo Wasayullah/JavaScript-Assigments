5629912d634a7a885df02b499c2fea19c75a8af9
Mohammad wasayullah <wasayullahmohammad@gmail.com>
2026-08-31 23:46:15 +0500

    completed latest assignments

--- app.js
+++ app.js
@@ -2170,11 +2170,13 @@ document.getElementById("ch-38_question_10").innerHTML = currencyNotes(amount);
 /*
  Chapters - 42 to 48 : EVENTS
 
- */
  // 1.  Show an alert box on click on a link.
-const btn = document.querySelector(".btn").addEventListener('click',()=>{
+ const btn = document.querySelector(".btn").addEventListener('click',()=>{
     alert("You clicked the button")
 })
+*/
+// 2. Display some Mobile images in browser. On click on an 
+// image Show the message in alert to user. 
   
 
 
