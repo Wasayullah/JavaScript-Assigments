cbf6b1d6c327d2f02090ec976840dce4338a8275
Mohammad wasayullah <wasayullahmohammad@gmail.com>
2026-08-31 23:39:02 +0500

    completed latest assignments

--- app.js
+++ app.js
@@ -2164,4 +2164,4 @@ function currencyNotes(amount) {
 const amount = Number(prompt("Enter amount to withdraw:"));
 document.getElementById("ch-38_question_10").innerHTML = currencyNotes(amount);
 */
-
+/*
