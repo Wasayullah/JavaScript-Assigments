15a7e2a5eda0568b9db4a370caa9285f38170782
Mohammad wasayullah <wasayullahmohammad@gmail.com>
2026-08-31 23:18:34 +0500

    completed latest assignments

--- app.js
+++ app.js
@@ -2141,7 +2141,7 @@ function calculateOvertime(hours) {
 console.log("Overtime Pay: Rs.", calculateOvertime(hours));
         */
 // 10. A cashier has currency notes of denominations 10, 50 and 
-100. If the amount to be withdrawn is input through the 
-keyboard in hundreds, find the total number of currency notes 
-of each denomination the cashier will have to give to the 
-withdrawer.
+// 100. If the amount to be withdrawn is input through the 
+// keyboard in hundreds, find the total number of currency notes 
+// of each denomination the cashier will have to give to the 
+// withdrawer.
