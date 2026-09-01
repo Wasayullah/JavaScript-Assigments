ed145b777f3890f0871afc937e2245e050d08167
Mohammad wasayullah <wasayullahmohammad@gmail.com>
2026-09-02 00:00:47 +0500

    completed latest assignments

--- app.js
+++ app.js
@@ -2197,7 +2197,7 @@ const op = document.querySelector(".op").addEventListener('click',()=>{
 
 const getDeletebutton = document.querySelector('.delete').addEventListener("click",()=>{
 
-const getRow = document.querySelector('.delete').parentNode
+const getRow = document.getElementById('delete').parentNode
 console.log(getRow)
 
 
