c12e6b33e7498f2d11565212d4740071f646a56b
Mohammad wasayullah <wasayullahmohammad@gmail.com>
2026-09-01 23:36:24 +0500

    completed latest assignments

--- app.js
+++ app.js
@@ -2191,4 +2191,6 @@ const op = document.querySelector(".op").addEventListener('click',()=>{
 });
 
 */
-//
+// 3. Display 10 student records in table and each row should contain a delete 
+button. If you click on a button to delete a record, entire row should be 
+deleted. 
