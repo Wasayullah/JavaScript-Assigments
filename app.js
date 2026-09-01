26c209edfb69fc6ce9d0e42a03d992064b861f8c
Mohammad wasayullah <wasayullahmohammad@gmail.com>
2026-09-01 23:50:47 +0500

    completed latest assignments

--- app.js
+++ app.js
@@ -2195,6 +2195,4 @@ const op = document.querySelector(".op").addEventListener('click',()=>{
 // button. If you click on a button to delete a record, entire row should be 
 // deleted. 
 
-const studentsRecords = ()=>{
-    
-}
+const getDeletebutton = document
