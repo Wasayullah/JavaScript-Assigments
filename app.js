58191c6a95845cafd7be20907758d48dacee0801
Mohammad wasayullah <wasayullahmohammad@gmail.com>
2026-09-02 00:16:23 +0500

    completed latest assignments

--- app.js
+++ app.js
@@ -2197,9 +2197,7 @@ const op = document.querySelector(".op").addEventListener('click',()=>{
 
 const getDeletebutton = document.querySelector('.delete').addEventListener("click",()=>{
 
-const row = button.closest('tr');
+const row = getDeletebutton.closest('tr');
 row.remove();
-
-
 
 });
