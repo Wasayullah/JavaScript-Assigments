88c8f346390b3ba4b255d782676f7af95d0f63e3
Mohammad wasayullah <wasayullahmohammad@gmail.com>
2026-09-05 22:16:55 +0500

    completed latest assignments

--- app.js
+++ app.js
@@ -2276,8 +2276,6 @@ counter();
 });
 
 */
-// 2. Suppose in your webpage there is content area in which 
-you have entered your item details, but user can only see 
-some details on first look. When user clicks on “Read 
-more” button, full detail of that particular item will be 
-displayed. 
+// 2. Suppose in your webpage there is content area in which you have entered your item details,
+//  but user can only see some details on first look. When user clicks on “Read more” button,
+//  full detail of that particular item will be displayed. 
