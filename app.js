e6178de9d3cb728f4d5fe20596c6d2b58fe91914
Mohammad wasayullah <wasayullahmohammad@gmail.com>
2026-09-07 23:56:22 +0500

    completed latest assignments

--- app.js
+++ app.js
@@ -2345,5 +2345,5 @@ let rowBeingEdited = null;
             document.getElementById("editAge").value = row.querySelector(".cAge").textContent;
             document.getElementById("editCourse").value = row.querySelector(".cCourse").textContent;
 
-            editForm.style.display = "block";
+    editForm.style.display = "block";
         }
