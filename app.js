021e504308c1c59327b9242f68550989b922d96c
Mohammad wasayullah <wasayullahmohammad@gmail.com>
2026-09-07 23:57:40 +0500

    completed latest assignments

--- app.js
+++ app.js
@@ -2314,10 +2314,10 @@ studentForm.addEventListener("submit", function (event) {
 function addRow(name, age, course) {
     const row = document.createElement("tr");
     row.innerHTML = `
+    <td class="cName">${name}</td>
+    <td class="cAge">${age}</td>
+    <td class="cCourse">${course}</td>
     */
-                <td class="cName">${name}</td>
-                 <td class="cAge">${age}</td>
-                <td class="cCourse">${course}</td>
                 <td>
                  <button class="editBtn">Edit</button>
                     <button class="deleteBtn">Delete</button>
