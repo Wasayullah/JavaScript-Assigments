f73bb7f10666b74d3800ac3ada970055ffb78509
Mohammad wasayullah <wasayullahmohammad@gmail.com>
2026-09-07 23:57:03 +0500

    completed latest assignments

--- app.js
+++ app.js
@@ -2295,8 +2295,8 @@ buttons.forEach(function (button) {
         }
     });
 });
-        */
 // 3. In previous assignment you have created a tabular data  using javascript. Let’s modify that. Create a form which 
+*/
 // takes student’s details and show each student detail in  table. Each row of table must contain a delete button and 
 // an edit button. On click on delete button entire row should be deleted. On click on edit button, a hidden form will 
 // appear with the values of that row. 
