4777b73122d99d79cb5238d39654113ea85c1126
Mohammad wasayullah <wasayullahmohammad@gmail.com>
2026-09-08 11:13:58 +0500

    completed latest assignments

--- app.js
+++ app.js
@@ -2381,4 +2381,4 @@ document.getElementById("cancelEditBtn").addEventListener("click", function () {
 // iv.  Fill input value whose element id first-name using javascript.
 // v. Repeat part iv for id ”last-name” and “email”.
 
-const main = document.getElementById("main-content")
+const main = document.getElementById("main-content").
