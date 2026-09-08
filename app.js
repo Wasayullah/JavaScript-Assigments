1489aa828d158cd55ebf3e95d65bc83060645ba9
Mohammad wasayullah <wasayullahmohammad@gmail.com>
2026-09-08 11:05:56 +0500

    completed latest assignments

--- app.js
+++ app.js
@@ -2368,4 +2368,4 @@ document.getElementById("cancelEditBtn").addEventListener("click", function () {
 //----------------------------------------------------------------------------------------------------------------
 
 /*
- Chapters - 58 to 52 : DOM
+ Chapters - 58 to 67 : DOM
