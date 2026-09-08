67a1fd07377708484a560957a419daccd16dc6f3
Mohammad wasayullah <wasayullahmohammad@gmail.com>
2026-09-08 11:15:24 +0500

    completed latest assignments

--- app.js
+++ app.js
@@ -2383,4 +2383,4 @@ document.getElementById("cancelEditBtn").addEventListener("click", function () {
 
 const main = document.getElementById("main-content")
     output=
-    `<p class = "fs-4 bg-darks"></p>`
+    `<p class = "fs-4"></p>`
