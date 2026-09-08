170acbd52eef03b072a3a1acca6e0ed5c0ecd3c7
Mohammad wasayullah <wasayullahmohammad@gmail.com>
2026-09-08 11:07:40 +0500

    completed latest assignments

--- app.js
+++ app.js
@@ -2374,9 +2374,9 @@ document.getElementById("cancelEditBtn").addEventListener("click", function () {
 
 // 1. Consider you have following code snippet:
 // (view html file)
-i. Get element of id “main-content” and assign them in a variable.
-ii. Display all child elements of “main-content” element.
-iii.  Get all elements of class “render” and show their innerHTML 
-in browser.
-iv.  Fill input value whose element id first-name using javascript.
-v. Repeat part iv for id ”last-name” and “email”.
+//i. Get element of id “main-content” and assign them in a variable.
+//ii. Display all child elements of “main-content” element.
+//iii.  Get all elements of class “render” and show their innerHTML 
+//in browser.
+//iv.  Fill input value whose element id first-name using javascript.
+//v. Repeat part iv for id ”last-name” and “email”.
